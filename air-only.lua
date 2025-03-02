b162b5adb3ac0e3570ff31b5caa53d124d289512
Will Harader <wharader@gmail.com>
2025-03-02 15:55:50 -0800

    Armada T3 factory fix

--- air-only.lua
+++ air-only.lua
@@ -1,6 +1,6 @@
 --Air Only
 for name, ud in pairs(UnitDefs) do
-    for index, value in ipairs({'corlab', 'corsy', 'corvp','corhp','corfhp','corgant','coramsub','armlab', 'armsy', 'armvp','armhp','armfhp','armgant','armamsub','leglab', 'legsy', 'legvp','leghp','legfhp','leggant','legamsub',}) do
+    for index, value in ipairs({'corlab', 'corsy', 'corvp','corhp','corfhp','corgant','coramsub','armlab', 'armsy', 'armvp','armhp','armfhp','armshltx','armamsub','leglab', 'legsy', 'legvp','leghp','legfhp','leggant','legamsub',}) do
         if name == value then
           ud.maxthisunit = 0
           break
