e185fb573f9e07726b4fda176aac75c65336c014
Will Harader <wharader@gmail.com>
2025-03-05 18:06:44 -0800

    mod resource links, nerf anti-air towers

--- air-only.lua
+++ air-only.lua
@@ -1,13 +1,25 @@
 --Air Only
 for name, ud in pairs(UnitDefs) do
-    for index, value in ipairs({
-        'corlab', 'corsy', 'corvp','corhp','corfhp','corgant','coramsub',
-        'armlab', 'armsy', 'armvp','armhp','armfhp','armshltx','armamsub',
-        'leglab', 'legsy', 'legvp','leghp','legfhp','leggant','legamsub',
-    }) do
-        if name == value then
-            ud.maxthisunit = 0
-            break
-        end
+  --Remove non-air factories
+  for index, value in ipairs({
+    'corlab', 'corsy', 'corvp','corhp','corfhp','corgant','coramsub',
+    'armlab', 'armsy', 'armvp','armhp','armfhp','armshltx','armamsub',
+    'leglab', 'legsy', 'legvp','leghp','legfhp','leggant','legamsub',
+  }) do
+    if name == value then
+      ud.maxthisunit = 0
+      break
     end
+  end
+  -- Nerf anti-air towers
+  if name == 'armmercury' then
+    ud.weapondefs.arm_advsam.damage.vtol = 250
+  end
+  if name == 'corscreamer' then
+    ud.weapondefs.cor_advsam.damage.vtol = 250
+  end
+  if name == 'leglraa' then
+    ud.weapondefs.cor_advsam.damage.vtol = 250
+    ud.weapondefs.railgunt2.damage.vtol = 250
+  end
 end
---
+++ modding-resources.md
@@ -0,0 +1,15 @@
+# Beyond All Reason Modding Resources
+
+[Tweak Options Guide by badosu](https://gist.github.com/badosu/f2617db52e7486a7769366642d354a01)
+
+[Zero-K Modding Guide](https://zero-k.info/mediawiki/Quick_Stat_Tweaks)
+
+[Unit List (in English)](https://github.com/beyond-all-reason/Beyond-All-Reason/blob/master/language/en/units.json)
+
+[Unit Definitions](https://github.com/beyond-all-reason/Beyond-All-Reason/tree/master/units)
+
+## Examples
+
+[badosu](https://gist.github.com/badosu/f2617db52e7486a7769366642d354a01#file-tweaks-md)
+
+[BAR Discord other-mods](https://discord.com/channels/549281623154229250/1129127537763692654)
